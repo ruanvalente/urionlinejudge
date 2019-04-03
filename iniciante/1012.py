6fe1ddab3c7dec6bd80169015b1ad7e978a60a79
Ruan Valente <ruanopensuselinux@gmail.com>
2019-04-02 22:47:01 -0300

    Adicionados os exercicios 1011 e 1012

---
+++ iniciante/1012.py
@@ -0,0 +1,26 @@
+"""
+Escreva um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C. Em seguida, calcule e mostre:
+a) a área do triângulo retângulo que tem A por base e C por altura.
+b) a área do círculo de raio C. (pi = 3.14159)
+c) a área do trapézio que tem A e B por bases e C por altura.
+d) a área do quadrado que tem lado B.
+e) a área do retângulo que tem lados A e B.
+Entrada
+O arquivo de entrada contém três valores com um dígito após o ponto decimal.
+
+Saída
+O arquivo de saída deverá conter 5 linhas de dados. Cada linha corresponde a uma das áreas descritas acima, sempre com mensagem correspondente e um espaço entre os dois pontos e o valor. O valor calculado deve ser apresentado com 3 dígitos após o ponto decimal.
+"""
+
+valor = input().split(" ")
+a, b, c = valor
+pi = 3.14159
+
+triangulo = (float(a) * float(c))/2
+circulo = pi * (float(c)* float(c))
+trapezio = float(c) *(float(a) + float(b)) / 2
+quadrado = float(b) * float(b)
+retangulo = float(a) * float(b)
+
+
+print("TRIANGULO: {:.3f}\nCIRCULO: {:.3f}\nTRAPEZIO: {:.3f}\nQUADRADO: {:.3f}\nRETANGULO: {:.3f}".format(triangulo, circulo, trapezio, quadrado, retangulo))
